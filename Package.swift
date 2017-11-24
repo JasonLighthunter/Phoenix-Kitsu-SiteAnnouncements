f67713dee7d82781d2f62bacfa0caa670f96e0f1
Job Cuppen <jobcuppen@live.nl>
2017-11-24 12:42:41 +0100

    added spm files

--- Package.swift
+++ Package.swift
@@ -10,8 +10,6 @@ let package = Package(
              targets: ["PhoenixKitsuSiteAnnouncements"])
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
     .package(url: "https://github.com/JasonLighthunter/Phoenix-Kitsu-Core.git", .branch("setup"))
   ],
   targets: [
